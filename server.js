d4b195d8b5e70e6f85113e91303912b5707be91d
Enes Elmal <eneselmal1994@gmail.com>
2022-02-23 17:59:29 +0300

    server and db connected, controllers created

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
 const knex = require('knex')
 
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -12,8 +17,6 @@ const db = knex({
     password: 'enes',
     database: 'smartbrain',
   },
-  // pool: { min: 0, max: 50 },
-  // debug: true,
 })
 
 const app = express()
@@ -26,89 +29,22 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-  db.select('email', 'hash')
-    .from('login')
-    .where('email', '=', req.body.email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
-      if (isValid) {
-        console.log(isValid)
-        return db
-          .select('*')
-          .from('users')
-          .where('email', '=', req.body.email)
-          .then((user) => {
-            res.json(user[0])
-          })
-          .catch((err) => res.status(400).json('unable to get user'))
-      } else {
-        res.status(400).json('wrong credentials')
-      }
-    })
-    .catch((err) => res.status(400).json('wrong credentials'))
+  signin.handleSignin(req, res, db, bcrypt)
 })
 
 app.post('/register', (req, res) => {
-  const { email, name, password } = req.body
-  const hash = bcrypt.hashSync(password)
-  db.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into('login')
-      .returning('email')
-      .then((loginEmail) => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-            // loginEmail[0] --> this used to return the email
-            // TO
-            // loginEmail[0].email --> this now returns the email
-            email: loginEmail[0],
-            name: name,
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.json(user[0])
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-  }).catch((err) => res.status(400).json('unable to register'))
+  register.handleRegister(req, res, db, bcrypt)
 })
 
 app.get('/profile/:id', (req, res) => {
-  const { id } = req.params
-  db.select('*')
-    .from('users')
-    .where({ id })
-    .then((user) => {
-      if (user.length) {
-        res.json(user[0])
-      } else {
-        res.status(400).json('Not found')
-      }
-    })
-    .catch((err) => res.status(400).json('error getting user'))
+  profile.handleProfileGet(req, res)
 })
 
 app.put('/image', (req, res) => {
-  const { id } = req.body
-  db('users')
-    .where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then((entries) => {
-      // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-      // entries[0] --> this used to return the entries
-      // TO
-      // entries[0].entries --> this now returns the entries
-      res.json(entries[0].entries)
-    })
-    .catch((err) => res.status(400).json('unable to get entries'))
+  image.handleImage(req, res, db)
+})
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res)
 })
 
 app.listen(3000, () => {
