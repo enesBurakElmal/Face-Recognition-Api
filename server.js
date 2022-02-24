d1bced6a55d13784a26697976fa9366d033209d4
eneselmal <eneselmal1994@gmail.com>
2022-02-24 14:03:19 +0300

    s

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = knex({
       process.env.DATABASE_URL ||
       'postgres://zsdncmfhtjyrdd:574eb7fba95eee735e9a8489d18d2ef00823dcd98c3b757e08cc16dc03959509@ec2-3-219-204-29.compute-1.amazonaws.com:5432/de2i88qqbl79qv',
     ssl: {
-      rejectUnauthorized: true,
+      rejectUnauthorized: false,
     },
   },
 })
